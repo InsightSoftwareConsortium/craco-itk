da58b1025c3700bcec8d2233b0fcaa58b3721a78
Piotr Delinowski <piotr@labelbox.com>
2023-09-28 23:22:55 +0200

    Upgrade copy-webpack-plugin to v11.0.0

--- index.js
+++ index.js
@@ -13,24 +13,26 @@ module.exports = () => ({
     const itkSource = path.join(paths.appNodeModules, 'itk');
     const itkDestination = path.join(paths.appBuild, 'itk');
     webpackConfig.plugins.push(
-      new CopyWebpackPlugin([
-        {
-          from: path.join(itkSource, 'WebWorkers'),
-          to: path.join(itkDestination, 'WebWorkers')
-        },
-        {
-          from: path.join(itkSource, 'ImageIOs'),
-          to: path.join(itkDestination, 'ImageIOs')
-        },
-        {
-          from: path.join(itkSource, 'MeshIOs'),
-          to: path.join(itkDestination, 'MeshIOs')
-        },
-        {
-          from: path.join(itkSource, 'PolyDataIOs'),
-          to: path.join(itkDestination, 'PolyDataIOs')
-        }
-      ])
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.join(itkSource, 'WebWorkers'),
+            to: path.join(itkDestination, 'WebWorkers')
+          },
+          {
+            from: path.join(itkSource, 'ImageIOs'),
+            to: path.join(itkDestination, 'ImageIOs')
+          },
+          {
+            from: path.join(itkSource, 'MeshIOs'),
+            to: path.join(itkDestination, 'MeshIOs')
+          },
+          {
+            from: path.join(itkSource, 'PolyDataIOs'),
+            to: path.join(itkDestination, 'PolyDataIOs')
+          }
+        ]
+      })
     );
 
     webpackConfig.resolve.alias['./itkConfig$'] = path.resolve(__dirname, 'itkConfig.js')
